1985ce9b1c7eee62f0e492f6a98fbdf11c6f1ec7
strawmr <mstrawjr@gmail.com>
2025-01-03 10:02:34 -0500

    updated meta

--- app.vue
+++ app.vue
@@ -2,15 +2,15 @@
 useHead({
   title: 'Video Game Layoffs | Track Gaming Industry Layoffs | Last Gaming Layoffs',
   meta: [
-    { name: 'description', content: 'Track the latest gaming layoffs in 2024. Get the most recent data on company layoffs and employee numbers.' },
-    { name: 'keywords', content: 'gaming layoffs, layoffs 2024, gaming industry, employee layoffs' },
-    { property: 'og:title', content: 'Gaming Layoff Tracker - 2024' },
+    { name: 'description', content: 'Track the latest gaming layoffs in 2025. Get the most recent data on company layoffs and employee numbers.' },
+    { name: 'keywords', content: 'gaming layoffs, layoffs 2025, gaming industry, employee layoffs' },
+    { property: 'og:title', content: 'Gaming Layoff Tracker - 2025' },
     { property: 'og:description', content: 'Track the latest gaming layoffs in 2024. Get the most recent data on company layoffs and employee numbers.' },
     { property: 'og:image', content: 'https://gaminglayoffs.com/_nuxt/PinkSlip.DHAW9k5K.png' }, // Replace with your image URL
     { property: 'og:url', content: 'https://gaminglayoffs.com' }, // Replace with your website URL
     { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:title', content: 'Gaming Layoff Tracker - 2024' },
-    { name: 'twitter:description', content: 'Track the latest gaming layoffs in 2024. Get the most recent data on company layoffs and employee numbers.' },
+    { name: 'twitter:title', content: 'Gaming Layoff Tracker - 2025' },
+    { name: 'twitter:description', content: 'Track the latest gaming layoffs in 2025. Get the most recent data on company layoffs and employee numbers.' },
     { name: 'twitter:image', content: 'https://gaminglayoffs.com/_nuxt/PinkSlip.DHAW9k5K.png' } // Replace with your image URL
   ],
   bodyAttrs: {
