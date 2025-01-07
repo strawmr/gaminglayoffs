dc8471852ccd491998caba9cd89c444325875f6a
strawmr <mstrawjr@gmail.com>
2025-01-07 13:17:50 -0500

    updated SEO

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 useHead({
-  title: 'Video Game Layoffs | Track Gaming Industry Layoffs | Last Gaming Layoffs',
+  title: '2025 Games Industry Layoffs | Video Game Layoffs',
   meta: [
-    { name: 'description', content: 'Track the latest gaming layoffs in 2025. Get the most recent data on company layoffs and employee numbers.' },
-    { name: 'keywords', content: 'gaming layoffs, layoffs 2025, gaming industry, employee layoffs' },
+    { name: 'description', content: 'Track the latest games industry layoffs in 2025. Get the most recent data on company layoffs, employee numbers, and locations.' },
+    { name: 'keywords', content: 'gaming layoffs, layoffs 2025, gaming industry, employee layoffs, games industry layoffs, games layoffs, video game layoffs' },
     { property: 'og:title', content: 'Gaming Layoff Tracker - 2025' },
     { property: 'og:description', content: 'Track the latest gaming layoffs in 2025. Get the most recent data on company layoffs and employee numbers.' },
     { property: 'og:image', content: 'https://gaminglayoffs.com/_nuxt/PinkSlip.DHAW9k5K.png' }, // Replace with your image URL
