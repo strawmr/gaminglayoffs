e467a389d2d0c947db8b1ee06c67a15ffdf20e3b
mike <mstrawjr@gmail.com>
2026-01-14 14:05:53 -0500

    updated year

--- app.vue
+++ app.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 useHead({
-  title: '2025 Games Industry Layoffs | Video Game Layoffs',
+  title: '2026 Games Industry Layoffs | Video Game Layoffs',
   meta: [
-    { name: 'description', content: 'Track the latest games industry layoffs in 2025. Get the most recent data on company layoffs, employee numbers, and locations.' },
-    { name: 'keywords', content: 'gaming layoffs, layoffs 2025, gaming industry, employee layoffs, games industry layoffs, games layoffs, video game layoffs' },
-    { property: 'og:title', content: 'Gaming Layoff Tracker - 2025' },
-    { property: 'og:description', content: 'Track the latest gaming layoffs in 2025. Get the most recent data on company layoffs and employee numbers.' },
+    { name: 'description', content: 'Track the latest games industry layoffs in 2026. Get the most recent data on company layoffs, employee numbers, and locations.' },
+    { name: 'keywords', content: 'gaming layoffs, layoffs 2025, layoffs 2026, gaming industry, employee layoffs, games industry layoffs, games layoffs, video game layoffs' },
+    { property: 'og:title', content: 'Gaming Layoff Tracker - 2026' },
+    { property: 'og:description', content: 'Track the latest gaming layoffs in 2026. Get the most recent data on company layoffs and employee numbers.' },
     { property: 'og:image', content: 'https://gaminglayoffs.com/_nuxt/PinkSlip.DHAW9k5K.png' }, // Replace with your image URL
     { property: 'og:url', content: 'https://gaminglayoffs.com' }, // Replace with your website URL
     { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:title', content: 'Gaming Layoff Tracker - 2025' },
-    { name: 'twitter:description', content: 'Track the latest gaming layoffs in 2025. Get the most recent data on company layoffs and employee numbers.' },
+    { name: 'twitter:title', content: 'Gaming Layoff Tracker - 2026' },
+    { name: 'twitter:description', content: 'Track the latest gaming layoffs in 2026. Get the most recent data on company layoffs and employee numbers.' },
     { name: 'twitter:image', content: 'https://gaminglayoffs.com/_nuxt/PinkSlip.DHAW9k5K.png' } // Replace with your image URL
   ],
   bodyAttrs: {
